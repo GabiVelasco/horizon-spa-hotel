bef71ac14a010004627584a5df3fb1ced8768800
GabrielaVelasco <gabriela.luppe@gmail.com>
2024-10-08 15:08:15 +0200

    animations for third and forth sections added.

--- script.js
+++ script.js
@@ -42,3 +42,53 @@ const secondSectionObserver = new IntersectionObserver((entries) => {
 // Beobachte die Elemente der zweiten Sektion
 const secondSectionElements = document.querySelectorAll('.content:nth-of-type(2) .animate');
 secondSectionElements.forEach(el => secondSectionObserver.observe(el));
+
+
+
+// Für die dritte Sektion, Threshold auf 0.5 setzen
+const thirdSectionOptions = {
+  root: null, // Beobachtet den gesamten Viewport
+  rootMargin: '0px', // Keine zusätzlichen Margen
+  threshold: 0.5 // 50% des Elements müssen sichtbar sein, um die Animation zu triggern
+};
+
+const thirdSectionObserver = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show'); // Animation auslösen
+    } else {
+      entry.target.classList.remove('show'); // Animation zurücksetzen, wenn es den Viewport verlässt
+    }
+  });
+}, thirdSectionOptions);
+
+
+// Beobachte die Elemente der zweiten Sektion
+const thirdSectionElements = document.querySelectorAll('.content:nth-of-type(3) .animate');
+thirdSectionElements.forEach(el => thirdSectionObserver.observe(el));
+
+
+
+
+// Für die dritte Sektion, Threshold auf 0.5 setzen
+const forthSectionOptions = {
+  root: null, // Beobachtet den gesamten Viewport
+  rootMargin: '0px', // Keine zusätzlichen Margen
+  threshold: 0.5 // 50% des Elements müssen sichtbar sein, um die Animation zu triggern
+};
+
+const forthSectionObserver = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show'); // Animation auslösen
+    } else {
+      entry.target.classList.remove('show'); // Animation zurücksetzen, wenn es den Viewport verlässt
+    }
+  });
+}, forthSectionOptions);
+
+
+// Beobachte die Elemente der zweiten Sektion
+const forthSectionElements = document.querySelectorAll('.content:nth-of-type(4) .animate');
+forthSectionElements.forEach(el => forthSectionObserver.observe(el));
+
